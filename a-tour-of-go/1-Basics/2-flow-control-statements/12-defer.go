ca766d9f961bdc3ba82ea90d1e354465cc8ab9d0
Felipe Acevedo <felipe.a.montoya@gmail.com>
2021-01-10 23:53:50 -0500

    Add A Tour of Go lessons from 9 to 13

---
+++ a-tour-of-go/1-Basics/2-flow-control-statements/12-defer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/* A defer statement defers the execution of a function until the surrounding
+   function returns.
+   
+   The deferred call's arguments are evaluated inmediately, but the function call
+   is not executed until the surrounding function returns.  */
+func main() {
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
+}
---
+++ a-tour-of-go/1-Basics/2-flow-control-statements/13-defer-multi.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/* Deferred function calls are pushed onto a stack. When a function returns, its
+   deferred calls are executed in last-in-first-out order. */
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
